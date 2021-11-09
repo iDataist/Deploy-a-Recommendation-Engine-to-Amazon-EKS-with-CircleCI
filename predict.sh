c8972138c371e9269109e1945f063bb2412c0d2c
Hui Ren <huiren16@gmail.com>
2021-11-09 14:28:27 -0600

    Update predict.sh

--- predict.sh
+++ predict.sh
@@ -1,3 +1,3 @@
 curl -d '{"user_id": 0, "nrec_items": 10}' \
      -H "Content-Type: application/json" \
-     -X POST http://192.168.41.156:80/predict
+     -X POST http://a98c06235550041629778132beb769fd-1924624268.us-east-1.elb.amazonaws.com/predict
